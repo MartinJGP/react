03a5c373935d932d73a1b0a6a65de693fb360430
Martin <martincasona085@gmail.com>
2024-12-01 11:24:41 -0500

    second

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CarruselServicios from './components/CarruselServicios';
 import ExperienciaServicio from './components/ExperienciaServicios';
 import PiePagina from './components/FootPage';
 import NavBar from './components/NavBar';
+import DataTable from './components/PruebaItems';
+import RegItems from './components/RegItems';
 import './App.css';
 
 function App() {
     return (
-        <div>
-            <NavBar/>
-            <CarruselServicios />
-            <ExperienciaServicio />
-            <PiePagina />
-        </div>
+        <Router>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<div><CarruselServicios /><ExperienciaServicio /><PiePagina /></div>} />
+                <Route path="/items" element={<DataTable />} />
+                <Route path="/reg" element={<RegItems/>} />
+            </Routes>
+        </Router>
     );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
-import { AppBar, Button, IconButton, Toolbar, Typography, Box } from '@mui/material';
+import {AppBar, Button, IconButton, Toolbar, Typography, Box} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import Logo from '../assets/Logo.png';
 import {GoogleLogin} from "@react-oauth/google"; // Adjust the path as necessary
+import { Link } from 'react-router-dom';
 
 function NavBar() {
     return (
         <AppBar position="fixed" sx={{ backgroundColor: '#2553cd' }}>
             <Toolbar>
-                <Box component="img" src={Logo} alt="Logo" sx={{ height: 40, marginRight: 2 }} />
+                <Box component={Link} to="/" sx={{ display: 'flex', alignItems: 'center' }}>
+                    <Box component="img" src={Logo} alt="Logo" sx={{ height: 40, marginRight: 2 }} />
+                </Box>
                 <Box sx={{ marginLeft: 'auto' }}>
                     <GoogleLogin ux_mode={"popup"} theme={"filled_blue"} shape={"pill"} text={"signin"} type={"standard"} size={"medium"}
                                  onSuccess={(credentialResponse) => { console.log(credentialResponse) }}
@@ -18,6 +21,9 @@ function NavBar() {
                     />
 
                 </Box>
+                <Button color="inherit" component={Link} to="/items">
+                    Items
+                </Button>
             </Toolbar>
         </AppBar>
     );//sx={{ marginLeft: 'auto' }}
